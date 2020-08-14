45beb1eec3d4735b4345dc5748deab01b45b3ef8
vinod170317 <38979089+vinod170317@users.noreply.github.com>
2020-08-14 08:17:34 +0000

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+#print("welcome to python world")
+print("welcometo data world")
+
+def checkOdd(x):
+        if(x %2 ==0):
+            print(str(x) + "is an even number")
+        else:
+            print(str(x) + " is an odd number")
+
+
+
+checkOdd(10)
